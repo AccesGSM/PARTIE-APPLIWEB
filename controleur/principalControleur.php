b068155aea4ec4e5dd783341a84a8e09e10aaa49
AccesGSM <accesgsm64@gmail.com>
2024-05-30 13:12:29 +0200

    Update principalControleur.php

--- controleur/principalControleur.php
+++ controleur/principalControleur.php
@@ -4,13 +4,24 @@ function principalControleur($action){
     $lesActions = [];
     $lesActions["defaut"] = "accueilControleur.php";
     $lesActions["reservation"] = "reservationControleur.php";
+    $lesActions["usagers"] = "usagersControleur.php";
+    $lesActions["acces"] = "accesControleur.php";
     $lesActions["verifierReservation"] = "verifierReservationControleur.php";
     $lesActions["crudReservation"] = "crudReservationControleur.php";
     $lesActions["rechercheReservation"] = "rechercheReservationControleur.php";
     $lesActions["supprimerReservation"] = "supprimerReservationControleur.php";
     $lesActions["formMiseAJourReservation"] = "recapReservationControleur.php";
+    $lesActions["formMiseAJourUsager"] = "recapUsagerControleur.php";
+    $lesActions["formMiseAJourAcces"] = "recapAccesControleur.php";
+    $lesActions["miseAJourUsager"] = "majUsagerControleur.php";
     $lesActions["miseAJourReservation"] = "majReservationControleur.php";
+    $lesActions["miseAJourAcces"] = "majAccesControleur.php";
     $lesActions["creerReservation"] = "creerReservationControleur.php";
+    $lesActions["creerUsager"]="creerUsagerControleur.php";
+    $lesActions["creerAcces"]="creerAccesControleur.php";
+    $lesActions["profil"] = "profilControleur.php";
+    $lesActions["connexion"] = "loginControleur.php";
+    $lesActions["deconnexion"] = "logoutControleur.php";
 
     if (array_key_exists($action, $lesActions)){
         return $lesActions[$action];
