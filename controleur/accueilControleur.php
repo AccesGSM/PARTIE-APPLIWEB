9dc9c3e0f4f6ed482e2f296fb39ab259afc99d50
AccesGSM <accesgsm64@gmail.com>
2024-06-12 12:34:54 +0200

    Delete controleur directory

--- controleur/accueilControleur.php
+++
@@ -1,6 +0,0 @@
-<?php
-include_once 'menuControleur.php';
-
-$titre = "Accueil - Accès GSM";
-include "vue/partials/entete.html.php";
-include "vue/accueil.html.php";
--- controleur/menuControleur.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-// creation du menu de recherche
-$menuRecherche = [];
-$menuRecherche[] = ["url" => "./?action=rechercheReservation&critere=nom", "label" => "Recherche par nom"];
-$menuRecherche[] = ["url" => "./?action=rechercheReservation&critere=telephone", "label" => "Recherche par numéro de téléphone"];
-$menuRecherche[] = ["url" => "./?action=rechercheReservation&critere=designation", "label" => "Recherche par accès"];
